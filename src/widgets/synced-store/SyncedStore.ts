cba6d15b6d85edc362c2a86a5befda8022621c3d
DESKTOP-TFNDFB9\adeni <adeniyiib360@gmail.com>
2024-08-29 13:40:00 +0100

    chore: Update WebSocketProvider URL in SyncedStore.ts

--- src/widgets/synced-store/SyncedStore.ts
+++ src/widgets/synced-store/SyncedStore.ts
@@ -10,7 +10,7 @@ export const store = syncedStore({ formData: {} as InitFormData });
 // Get the Yjs document and sync automatically using y-webrtc
 const doc = getYjsDoc(store);
 // const webrtcProvider = new WebrtcProvider("form-synced-store", doc);
-export const wsProvider = new WebsocketProvider("ws://localhost:3000/api/socket", "form-synced-store", doc);
+export const wsProvider = new WebsocketProvider("ws://localhost:8000", "form-synced-store", doc);
 
 // export const disconnect = () => webrtcProvider.disconnect();
 // export const connect = () => webrtcProvider.connect();
