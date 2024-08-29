fe7dd7eb728efc4b2cea225567a1d234909bf3d0
DESKTOP-TFNDFB9\adeni <adeniyiib360@gmail.com>
2024-08-29 12:02:19 +0100

    chore: Add y-websocket dependency and update SyncedStore
    The y-websocket dependency has been added to the package.json file. Additionally, the SyncedStore.ts file has been updated to import and use the WebsocketProvider from y-websocket instead of the WebrtcProvider from y-webrtc.
    
    Note: Removed unnecessary code comments and unused imports.

--- src/widgets/synced-store/SyncedStore.ts
+++ src/widgets/synced-store/SyncedStore.ts
@@ -1,5 +1,6 @@
 import { syncedStore, getYjsDoc } from "@syncedstore/core";
 import { WebrtcProvider } from "y-webrtc";
+import { WebsocketProvider } from "y-websocket";
 
 type InitFormData = { data: string; };
 
@@ -8,7 +9,8 @@ export const store = syncedStore({ formData: {} as InitFormData });
 
 // Get the Yjs document and sync automatically using y-webrtc
 const doc = getYjsDoc(store);
-const webrtcProvider = new WebrtcProvider("form-synced-store", doc);
+// const webrtcProvider = new WebrtcProvider("form-synced-store", doc);
+export const wsProvider = new WebsocketProvider("ws://localhost:3000/api/socket", "form-synced-store", doc);
 
-export const disconnect = () => webrtcProvider.disconnect();
-export const connect = () => webrtcProvider.connect();
+// export const disconnect = () => webrtcProvider.disconnect();
+// export const connect = () => webrtcProvider.connect();
