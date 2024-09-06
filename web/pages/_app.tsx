1c7a16eedbeab222817bb4532907c3f7e49e6214
DESKTOP-TFNDFB9\adeni <adeniyiib360@gmail.com>
2024-09-06 14:14:35 +0100

    chore: import context suite globals and wrap component with same layout

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app';
 import '../src/styles/main.less';
+import { Layout } from 'antd';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Layout.Content style={{ minHeight: '100vh' }}>
+        <Component {...pageProps} />
+      </Layout.Content>
+    </Layout>
+  );
 }
